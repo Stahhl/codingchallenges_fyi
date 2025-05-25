8d2fd907975db666bab0cd76b29229874207b1ae
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2025-05-25 18:32:58 +0200

    vitest

---
+++ src/2-json-parser/ts/test/main.test.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { describe, it, expect } from 'vitest';
+
+const input = readFileSync(
+  resolve(process.cwd(), 'test/tests/step1/valid.json'),
+  'utf-8'
+);
+
+describe('Test something with input', () => {
+  it('should parse JSON correctly', () => {
+    const data = JSON.parse(input);
+    console.log(data)
+    expect(data).toBeDefined();
+    // Your assertions here
+  });
+});
