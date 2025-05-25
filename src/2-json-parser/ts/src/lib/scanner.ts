afb1dc74b2d0c858c49745c0bda69f19b86dc240
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2025-05-25 16:27:29 +0200

    starting on 2

---
+++ src/2-json-parser/ts/src/lib/scanner.ts
@@ -0,0 +1,52 @@
+export class Scanner {
+  private tokens: Token[] = [];
+  private source: string;
+  private start = 0;
+  private current = 0;
+
+  constructor(data: string) {
+    this.source = data;
+  }
+
+  scan(): Token[] {
+    while (this.current < this.source.length) {
+      this.start = this.current
+      this.scanToken()
+    }
+
+    return this.tokens;
+  }
+
+  private scanToken() {
+    const c = this.advance();
+
+    switch (c) {
+      case "{": {
+        this.tokens.push({ type: "brace_left", lexeme: "{", literal: null })
+        break
+      }
+      case "}": {
+        this.tokens.push({ type: "brace_right", lexeme: "}", literal: null })
+        break
+      }
+      default: {
+        break
+      }
+    }
+  }
+
+  private advance(): string {
+    return this.source[this.current++];
+  }
+}
+
+type TokenType =
+  | "brace_left"
+  | "brace_right";
+
+type Token = {
+  type: TokenType;
+  lexeme: string;
+  // deno-lint-ignore no-explicit-any
+  literal: any;
+};
--- src/2-json-parser/ts/test/main_test.ts
+++ src/2-json-parser/ts/test/main_test.ts
@@ -1,6 +1,16 @@
-import { assertEquals } from "@std/assert";
-import { sum } from "../src/lib/parser.ts"
+import { assert } from "@std/assert";
+import { Scanner } from "../src/lib/scanner.ts";
 
-Deno.test(function addTest() {
-  assertEquals(sum(2, 3), 5);
-});
+
+Deno.test(function test() {
+  const input = Deno.readTextFileSync(Deno.cwd() + "/test/tests/step1/valid.json")
+
+  const scanner = new Scanner(input);
+  const tokens = scanner.scan();
+
+  tokens.forEach(element => {
+    console.log(JSON.stringify(element))
+  });
+
+  assert(tokens.length > 0)
+})
