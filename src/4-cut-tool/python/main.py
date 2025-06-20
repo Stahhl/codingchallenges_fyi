7ed83310af981f57eb5656d6206e82825907a88a
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2025-06-20 13:57:35 +0200

    step 2

--- src/4-cut-tool/python/main.py
+++ src/4-cut-tool/python/main.py
@@ -4,16 +4,13 @@ from lib import *
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-f", dest="field", help="field number", nargs="?")
+parser.add_argument("-d", dest="delim", help="delimiter", nargs="?", required=False, default="\t")
 parser.add_argument("input_file", help="Path of the input file")
 
 args = parser.parse_args()
 
-print(args.field)
-print(args.input_file)
-
 content = read_file(args.input_file).decode("utf-8")
-print(content)
 
-split = split_str(content)
+split = split_str(content, args.delim)
 
 print(split[int(args.field)])
